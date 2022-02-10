662ce52d7bc0ed4cdb274c4b7c5c8706acc36b92
sahid25 <ashrafsahid25@gmail.com>
2022-02-10 19:23:47 +0600

    new commit

---
+++ templateString.js
@@ -0,0 +1,7 @@
+
+const firstName = 'Jhon';
+const lastName = 'hub';
+// const fullName = firstName + ' '+ lastName;
+const fullName = `${firstName} ${lastName} is a good boy;`
+console.log(object);
+console.log(fullName);
